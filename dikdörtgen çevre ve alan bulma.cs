861688c4f7aaaa41e729c30e1316eca71d220054
senasafa <senasafa8@gmail.com>
2024-11-21 23:49:57 +0300

    Add files via upload

---
+++ dikdörtgen çevre ve alan bulma.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        {
+            int kenar1, kenar2,cevre,alan;
+
+            Console.WriteLine("kenar1");
+            kenar1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("kenar2");
+            kenar2 = Convert.ToInt32(Console.ReadLine());
+
+           
+            cevre = 2 * (kenar1 + kenar2);
+            Console.WriteLine("cevre");
+
+            Console.WriteLine(cevre);
+            
+            alan = kenar1 * cevre;
+            Console.WriteLine("alan");
+            Console.WriteLine(alan);
+            
+            
+            
+            
+        }
+    }
+
+}
